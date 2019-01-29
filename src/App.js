dc10e8476236344871c90674b099a40834528ec8
vmunguia <mungassey@gmail.com>
2019-01-28 23:21:32 -0600

    First react app, hello world

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+		Hello world from React.js !!
+      </div>
+    );
+  }
+}
+
+export default App;
