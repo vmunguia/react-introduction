0986b6dba63705df10c319bc58a9a9df463f361d
vmunguia <mungassey@gmail.com>
2019-02-12 23:13:13 -0600

    working with state
    working with state

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {qty: 0};
+	}
+	
 	buy: function = () => {
-		alert("You bought an Android mobile.");
+		this.setState({qty: this.state.qty + 1});
+		//alert("You bought an Android mobile.");
 	}
 
 	render() {
@@ -10,6 +16,7 @@ class Product extends Component {
 			<div>
 				<p>Android - $199</p>
 				<button onClick={this.buy}>Buy</button>
+				<h3>Qty: {this.state.qty} item(s)</h3>
 			</div>
 		);
 	}
