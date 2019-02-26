6231ecc653b1435188043e901e7c509571a719f8
vmunguia <mungassey@gmail.com>
2019-02-25 20:50:00 -0600

    working with property - function
    passed as parameter as function as property - function

--- src/Product.jsx
+++ src/Product.jsx
@@ -10,12 +10,17 @@ class Product extends Component {
 		this.setState({qty: this.state.qty + 1});
 		//alert("You bought an Android mobile.");
 	}
+	
+	show: function = () => {
+		this.props.handleShow(this.props.name);
+	}
 
 	render() {
 		return (
 			<div>
 				<p>{this.props.name} - ${this.props.price}</p>
 				<button onClick={this.buy}>Buy</button>
+				<button onClick={this.show}>Show</button>
 				<h3>Qty: {this.state.qty} item(s)</h3>
 				<hr/>
 			</div>
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,12 +3,16 @@ import Product from './Product';
 import Total from './Total';
 
 class ProductList extends Component {
+	showProduct: function = (name) => {
+		alert("You selected " + name);
+	}
+
 	render() {
 		return (
 			<div>
-				<Product name="Android" price="101"/>
-				<Product name="Apple" price="123"/>
-				<Product name="Nokia" price="69"/>
+				<Product name="Android" price="101" handleShow={this.showProduct}/>
+				<Product name="Apple" price="123" handleShow={this.showProduct}/>
+				<Product name="Nokia" price="69" handleShow={this.showProduct}/>
 				<Total/>
 			</div>
 		);
